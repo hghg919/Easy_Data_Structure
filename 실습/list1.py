3f73323525be102d888e3672db0878a11537ff30
hghg919 <tgeq1023@naver.com>
2025-09-22 11:46:48 +0900

    (2025.09.22) 4주차
    실습 첫 수업
    리스트

---
+++ 실습/list1.py
@@ -0,0 +1,31 @@
+# print("hello world!")
+ 
+# a = []
+# b = [2,5,7]
+# a = {}
+# type(a)
+# a= []
+# type(a)
+# print(b)
+
+
+# a =[1,3,5,6,7]
+# a.insert(1,'mon')
+# print(a)
+# [1, 'mon', 3, 5, 6, 7]
+# a= [1,3,5,6,7]
+# a.append('mon')
+# print(a)
+# [1, 3, 5, 6, 7, 'mon']
+
+
+# a = [1,3,5,6,7]
+# a.pop(3)
+# a.pop(4)
+# a.pop()
+# a.pop(-1)
+# a.pop(-2)
+# a.remove(5)
+# print(a)
+
+
